78c83b8cb745fbf36373841f1071ce8b60137ebc
peachykish <nakisha.carroll@gmail.com>
2020-07-17 12:45:11 -0400

    first commit

--- code.js
+++ code.js
@@ -15,11 +15,12 @@ let sentence = ["The", "dog", "jumped", "over", "the", "fence"];
 // Log the word "jumped" to the console by accessing it in the array.
 
 // Your Code Here
-
+let myArray = [sentence]
+console.log(sentence[2]);
 // Step Two ------------------
 // Log the last word in the array to the console.
-
 // Your Code Here
+console.log(sentence[5])
 
 /*
 ---------------------------------------------------------------------------
@@ -34,7 +35,9 @@ Then log the sum to the console. (Hint: It should be 28)
 let numbers = [3, 4, 5, 7, 9];
 
 // Your Code Here
-
+console.log(3 + 4 + 5 + 7 + 9);
+let sumOfNumbers = 28
+console.log(sumOfNumbers)
 /*
 ---------------------------------------------------------------------------
 Exercise Three - Creating an array
@@ -46,9 +49,12 @@ Exercise Three - Creating an array
 // The array should be assigned to a variable named "colors"
 
 // Your Code Here
-
+myArray = [ "pink", "yellow", "green", "brown", "black"]
+let colors = myArray
+console.log(colors);
 // Step Two ------------------
 // Log the length of the array to the console (it should print 5)
+console.log(colors.length)
 
 /*
 ---------------------------------------------------------------------------
@@ -61,14 +67,16 @@ let names = []; // Don't modify this line.
 // Step One ------------------
 // Add 5 names of people to the array.
 // Then log your array to the console.
-
 // Your Code Here
+myArray = [ "Nakisha", "January", "Casey", "Nisha", "Edgar"]
+console.log(myArray)
 
 // Step Two ------------------
 // Using the index variable, assign a new name to the given index.
 // Then log the modified array to the console.
-let index = 2;
-
+let index = 2
+myArray[index] = "Banana"
+console.log(myArray)
 // Your Code Here
 
 /*
@@ -82,7 +90,9 @@ You will need to assign the result to a new variable.
 Then log the new array to the console.   Hint: Use concat, this was shown in the reading.
 */
 
-let firstArray = ["This", "will", "make"];
-let secondArray = ["a", "combined", "array."];
+let firstArray = [" This ", " will ", " make "];
+let secondArray = [" a ", " combined ", " array."];
 
 // Your Code Here.
+let phrase = firstArray + secondArray
+console.log(phrase)
